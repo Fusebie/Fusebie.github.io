f33889259789b44db4f1547e6476760f6bc39c59
Fusebie <fusebie117@gmail.com>
2025-04-28 18:37:56 +0530

    hompage and games page update, organisation
    new homepage themes, removed discord promo from every game's about, organized image assets, better naming for IDs and classes.

--- Assets/scripts/index_script.js
+++ Assets/scripts/index_script.js
@@ -1,45 +1,51 @@
-// var rate1isPressed = false;
-// var rate2isPressed = false;
-// var rate3isPressed = false;
-// var rate4isPressed = false;
-// var rate5isPressed = false;
-// var rate6isPressed = false;
+var backgroundImagePaths = ["Assets/images/backgrounds/corridor.png", "Assets/images/backgrounds/snow.png", "Assets/images/backgrounds/forest.png"];
 
+var headerContainer = document.getElementById("header-container");
+var h1_Fusebie = document.getElementById("h1-fusebie");
+var h2_welcome = document.getElementById("h2-welcome");
 
-// document.getElementById("btn-rate1").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate1isPressed = true;
-//     }
-    
-// }
-// document.getElementById("btn-rate2").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate2isPressed = true;
-//     }
-// }
-// document.getElementById("btn-rate3").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate3isPressed = true;
-//     }
-// }
-// document.getElementById("btn-rate4").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate4isPressed = true;
-//     }
-// }
-// document.getElementById("btn-rate5").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate5isPressed = true;
-//     }
-// }
-// document.getElementById("btn-rate6").onclick = function() {
-//     if (!rate1isPressed && !rate2isPressed && !rate3isPressed && !rate4isPressed && !rate5isPressed && !rate6isPressed) {
-//         this.textContent = "Thanks";
-//         rate6isPressed = true;
-//     }
-// }
+function changeTheme(randomInt)
+{
+    var backgroundimgURL = backgroundImagePaths[randomInt];
+    var backgroundImageValue = `url( ${backgroundimgURL} )`; 
+
+    if (randomInt == 0)
+    {
+        headerContainer.style.backgroundImage = backgroundImageValue;
+        headerContainer.style.backgroundPositionY = "-250px"
+
+        h1_Fusebie.style.color = "white";
+        h1_Fusebie.style.textShadow = "none";
+        
+        h2_welcome.style.color = "rgb(109, 219, 255)";
+    }
+    if (randomInt == 1)
+    {
+        headerContainer.style.backgroundImage = backgroundImageValue;
+        headerContainer.style.backgroundPositionY = "-100px"
+        
+        h1_Fusebie.style.color = "black";
+        h1_Fusebie.style.textShadow = "-1px -1px 0 rgb(255, 255, 255), 1px -1px 0 rgb(255, 255, 255), -1px 1px 0 rgb(255, 255, 255), 1px 1px 0 rgb(255, 255, 255)";
+
+        h2_welcome.style.color = "rgb(243, 243, 243)";
+    }
+    if (randomInt == 2)
+    {
+        headerContainer.style.backgroundImage = backgroundImageValue;
+        headerContainer.style.backgroundPositionY = "-150px"
+        
+        h1_Fusebie.style.color = "white";
+        h1_Fusebie.style.textShadow = "none";
+
+        h2_welcome.style.color = "rgb(109, 219, 255)";
+    }
+}
+
+function SetRandomTheme()
+{
+    changeTheme(getRndInteger(0, backgroundImagePaths.length - 1));
+}
+
+function getRndInteger(minInclusive, maxInclusive) {
+    return Math.floor(Math.random() * (maxInclusive - minInclusive + 1) ) + minInclusive;
+  }
